277718c60ae9551e0a1ec0e06e94843e2d7c6f13
Beat Scherrer <beat.scherrer@gmail.com>
2022-12-10 18:23:34 +0100

    update bash theme

--- beat.theme.sh
+++ beat.theme.sh
@@ -22,6 +22,13 @@
 #
 # shellcheck disable=SC2034,SC2154
 
+DEBUG=0
+debug() {
+  if [[ ${DEBUG} -ne 0 ]]; then
+    echo >&2 -e $*
+  fi
+}
+
 SCM_NONE_CHAR=''
 SCM_THEME_PROMPT_DIRTY=" ${_omb_prompt_brown}✗"
 SCM_THEME_PROMPT_CLEAN=""
@@ -38,31 +45,233 @@ THEME_CLOCK_FORMAT=${THEME_CLOCK_FORMAT:-"%I:%M:%S"}
 VIRTUALENV_THEME_PROMPT_PREFIX='('
 VIRTUALENV_THEME_PROMPT_SUFFIX=') '
 
-function _omb_theme_PROMPT_COMMAND() {
-  # This needs to be first to save last command return code
-  local RC="$?"
+# SEGMENT_SEPARATOR=''
+SEGMENT_SEPARATOR=''
+# RIGHT_SEPARATOR=''
+RIGHT_SEPARATOR=''
+LEFT_SUBSEG=''
+RIGHT_SUBSEG=''
 
-  local hostname="${_omb_prompt_bold_gray}\u \h"
-  local python_venv
-  _omb_prompt_get_python_venv
-  python_venv=$_omb_prompt_white$python_venv
+text_effect() {
+  case "$1" in
+  reset) echo 0 ;;
+  bold) echo 1 ;;
+  underline) echo 4 ;;
+  esac
+}
 
-  schroot_env=""
-  if [ -n "$SCHROOT_ALIAS_NAME" ]; then
-    schroot_env="${_omb_prompt_teal}[${SCHROOT_ALIAS_NAME}]"
+fg_color() {
+  case "$1" in
+  black) echo 30 ;;
+  red) echo 31 ;;
+  green) echo 32 ;;
+  yellow) echo 33 ;;
+  blue) echo 34 ;;
+  magenta) echo 35 ;;
+  cyan) echo 36 ;;
+  white) echo 37 ;;
+  orange) echo 38\;5\;166 ;;
+  esac
+}
+
+bg_color() {
+  case "$1" in
+  black) echo 40 ;;
+  red) echo 41 ;;
+  green) echo 42 ;;
+  yellow) echo 43 ;;
+  blue) echo 44 ;;
+  magenta) echo 45 ;;
+  cyan) echo 46 ;;
+  white) echo 47 ;;
+  orange) echo 48\;5\;166 ;;
+  esac
+}
+
+ansi() {
+  local seq
+  declare -a mycodes=("${!1}")
+
+  debug "ansi: ${!1} all: $* aka ${mycodes[@]}"
+
+  seq=""
+  for ((i = 0; i < ${#mycodes[@]}; i++)); do
+    if [[ -n $seq ]]; then
+      seq="${seq};"
+    fi
+    seq="${seq}${mycodes[$i]}"
+  done
+  debug "ansi debug:" '\\[\\033['${seq}'m\\]'
+  echo -ne '\[\033['${seq}'m\]'
+  # PR="$PR\[\033[${seq}m\]"
+}
+
+prompt_segment() {
+  local bg fg
+  declare -a codes
+
+  debug "Prompting $1 $2 $3"
+
+  # if commented out from kruton's original... I'm not clear
+  # if it did anything, but it messed up things like
+  # prompt_status - Erik 1/14/17
+
+  #    if [[ -z $1 || ( -z $2 && $2 != default ) ]]; then
+  codes=("${codes[@]}" $(text_effect reset))
+  #    fi
+  if [[ -n $1 ]]; then
+    bg=$(bg_color "$1")
+    codes=("${codes[@]}" "$bg")
+    debug "Added $bg as background to codes"
+  fi
+  if [[ -n $2 ]]; then
+    fg=$(fg_color "$2")
+    codes=("${codes[@]}" "$fg")
+    debug "Added $fg as foreground to codes"
   fi
 
-  # Set return status color
-  if [[ ${RC} == 0 ]]; then
-    ret_status="${_omb_prompt_bold_green}"
+  debug "Codes: "
+  # declare -p codes
+
+  if [[ $CURRENT_BG != NONE && "$1" != "$CURRENT_BG" ]]; then
+    declare -a intermediate=($(fg_color "$CURRENT_BG") $(bg_color "$1"))
+    debug "pre prompt " "$(ansi intermediate[@])"
+    PR="$PR$(ansi intermediate[@])$SEGMENT_SEPARATOR"
+    debug "post prompt " "$(ansi codes[@])"
+    PR="$PR$(ansi codes[@])"
   else
-    ret_status="${_omb_prompt_bold_brown}"
+    debug "no current BG, codes is $codes[@]"
+    PR="$PR$(ansi codes[@])"
+  fi
+  CURRENT_BG=$1
+  [[ -n $3 ]] && PR="$PR$3"
+}
+
+prompt_right_segment() {
+  local bg fg
+  declare -a codes
+
+  debug "Prompt right"
+  debug "Prompting $1 $2 $3"
+
+  # if commented out from kruton's original... I'm not clear
+  # if it did anything, but it messed up things like
+  # prompt_status - Erik 1/14/17
+
+  #    if [[ -z $1 || ( -z $2 && $2 != default ) ]]; then
+  codes=("${codes[@]}" $(text_effect reset))
+  #    fi
+  if [[ -n $1 ]]; then
+    bg=$(bg_color $1)
+    codes=("${codes[@]}" $bg)
+    debug "Added $bg as background to codes"
+  fi
+  if [[ -n $2 ]]; then
+    fg=$(fg_color $2)
+    codes=("${codes[@]}" $fg)
+    debug "Added $fg as foreground to codes"
   fi
 
-  # Append new history lines to history file
-  history -a
+  debug "Right Codes: "
+  # declare -p codes
 
-  PS1="$schroot_env$python_venv${hostname} ${_omb_prompt_bold_teal}\W $(scm_prompt_char_info)${ret_status}→ ${_omb_prompt_normal}"
+  # right always has a separator
+  # if [[ $CURRENT_RBG != NONE && $1 != $CURRENT_RBG ]]; then
+  #     $CURRENT_RBG=
+  # fi
+  declare -a intermediate2=($(fg_color $1) $(bg_color $CURRENT_RBG))
+  # PRIGHT="$PRIGHT---"
+  debug "pre prompt " $(ansi intermediate2[@])
+  PRIGHT="$PRIGHT$(ansi intermediate2[@])$RIGHT_SEPARATOR"
+  debug "post prompt " $(ansi codes[@])
+  PRIGHT="$PRIGHT$(ansi codes[@])"
+  # else
+  #     debug "no current BG, codes is $codes[@]"
+  #     PRIGHT="$PRIGHT$(ansi codes[@]) "
+  # fi
+  CURRENT_RBG=$1
+  [[ -n $3 ]] && PRIGHT="$PRIGHT$3"
+}
+
+prompt_end() {
+  if [[ -n $CURRENT_BG ]]; then
+    declare -a codes=($(text_effect reset) $(fg_color $CURRENT_BG))
+    PR="$PR$(ansi codes[@])$SEGMENT_SEPARATOR"
+  fi
+  declare -a reset=($(text_effect reset))
+  PR="$PR$(ansi reset[@]) "
+  CURRENT_BG=''
+}
+
+prompt_context() {
+  local user=$(whoami)
+
+  if [[ "$user" != "$DEFAULT_USER" || -n "$SSH_CLIENT" ]]; then
+    prompt_segment black default "${_omb_prompt_bold_gray}\u \h "
+  fi
+}
+
+prompt_schroot() {
+  if [[ -n "$SCHROOT_CHROOT_NAME" ]]; then
+    prompt_segment default red "($SCHROOT_CHROOT_NAME) "
+  fi
+}
+
+prompt_dir() {
+  prompt_segment default blue '\W '
+}
+
+git_status_dirty() {
+  dirty=$(git status -s 2>/dev/null | tail -n 1)
+  [[ -n $dirty ]] && echo " ●"
+}
+
+prompt_git() {
+  local ref dirty
+  if git rev-parse --is-inside-work-tree &>/dev/null; then
+    dirty=$(git_status_dirty)
+    ref=$(git symbolic-ref HEAD 2>/dev/null) || ref="➦ $(git show-ref --head -s --abbrev | head -n1 2>/dev/null)"
+    if [[ -n $dirty ]]; then
+      prompt_segment default yellow
+    else
+      prompt_segment default green
+    fi
+    PR="$PR${ref/refs\/heads\// }$dirty "
+  fi
+}
+
+prompt_status() {
+  if [[ $RETVAL == 0 ]]; then
+    prompt_segment default green "→ "
+  else
+    prompt_segment default red "→ "
+  fi
+}
+
+build_prompt() {
+  prompt_schroot
+  prompt_context
+  prompt_dir
+  prompt_git
+  prompt_status
+}
+
+_omb_theme_PROMPT_COMMAND() {
+  RETVAL=$?
+  PR=""
+  PRIGHT=""
+
+  build_prompt
+
+  # attempt to work around the ansi colors
+  local save='\e[s'
+  local rest='\e[u'
+  PRIGHT_stripped=$(sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" <<<"$PRIGHT")
+
+  PS1="$PR"
+
+  # PS1="\[${save}\e[${COLUMNS:-$(tput cols)}C\e[${#PS1RHS_stripped}D${PRIGHT}${rest}\]${PR}"
+  # PS1=$(printf "%*s\r%s\n\$ " "$(tput cols)" "$PRIGHT" "$PR")
 }
 
 _omb_util_add_prompt_command _omb_theme_PROMPT_COMMAND
